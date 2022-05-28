ddc0874bf23474465e63284d75d1f50c8c0f6357
Mr.Susheel Kumar <74233895+Susheel4115@users.noreply.github.com>
2022-05-28 10:52:54 +0530

    Update Post.js

--- Assignment_5/models/Post.js
+++ Assignment_5/models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
+// const { Schema } = mongoose;
+//const bcrypt = require("bcrypt");
 
 //schema for post
 
